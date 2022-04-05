1d9805a54b8ee6ec1af4e67dacdd5bb028bc2644
shahnawaz-pabon <s.pabon93@gmail.com>
2022-04-05 21:50:03 +0600

    go websocket application implemented with single stage dockerization

--- docker/single-stage/README.md
+++ docker/single-stage/README.md
@@ -1,5 +1,23 @@
-## Up and Run
+## Manually Up and Run
 
 ```sh
 go run main.go
 ```
+
+## Via docker
+
+```sh
+docker build -t go-websocket .
+docker run -it -p 8080:8080 go-websocket
+```
+
+Open up [http://localhost:8080](http://localhost:8080) in your browser and you'll be able to see `Home Page` message which is returned from the Go application.
+
+If you open up `index.html` in your browser, you'll see these two messages on the browser console.
+
+```sh
+Attempting Connection...
+Successfully Connected
+```
+
+and `Hi From the Client!` message on the running terminal.
--- docker/single-stage/main.go
+++ docker/single-stage/main.go
@@ -4,14 +4,69 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+// We'll need to define an Upgrader
+// this will require a Read and Write buffer size
+// This will hold information such as the Read and Write buffer size for our WebSocket connection
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home Page")
 }
 
+// define a reader which will listen for new messages being sent to our WebSocket endpoint
+// We’ll call this reader() for now and it will take in a pointer to the WebSocket connection
+// that we received from our call to upgrader.Upgrade:
+func reader(conn *websocket.Conn) {
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// print out that message for clarity
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	// fmt.Fprintf(w, "Hello World")
+
+	// This will determine whether or not an incoming request from a different domain is allowed to connect, and if it isn’t they’ll be hit with a CORS error.
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// upgrade this connection to a WebSocket connection
+	// upgrader.Upgrade() function which will take in the Response Writer
+	// and the pointer to the HTTP Request and return us with a pointer to a WebSocket connection,
+	// or an error if it failed to upgrade.
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	// helpful log statement to show connections
+	log.Println("Client Connected")
+
+	err = ws.WriteMessage(1, []byte("Hi Client!"))
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	// listen indefinitely for new messages coming
+	// through on our WebSocket connection
+	reader(ws)
 }
 
 func setupRoutes() {
